213776568530210dd11a25723f02abf524e68f1b
Tshwn <tasuke8@gmail.com>
2025-10-27 21:35:04 +0900

    small change

--- app/Http/Controllers/OAuthController.php
+++ app/Http/Controllers/OAuthController.php
@@ -8,11 +8,17 @@ class OAuthController extends Controller
 {
     public function googleAuth()
     {
-
+        return response()->json([
+            'oauth_service' => 'google',
+            'oauth_id' => $oauth_id,
+        ]);
     }
 
     public function appleAuth()
     {
-
+        return response()->json([
+            'oauth_service' => 'google',
+            'oauth_id' => $oauth_id,
+        ]);
     }
 }
