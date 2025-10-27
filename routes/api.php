1402c3a9ba951ab9b356c0f674e63f422cfe0fa0
Tshwn <tasuke8@gmail.com>
2025-10-27 21:23:49 +0900

    make oauth controller

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\OidcController;
+use App\Http\Controllers\OAuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('api')->group(function () {
@@ -9,4 +10,7 @@ Route::middleware('api')->group(function () {
     Route::middleware('auth.token')->group(function () {
         Route::get('/me', [OidcController::class, 'me']);
     });
+
+    Route::post('/auth/google', [OAuthController::class, 'googleAuth'])->name('auth.google');
+    Route::post('/auth/apple', [OAuthController::class, 'appleAuth'])->name('auth.apple');
 });
